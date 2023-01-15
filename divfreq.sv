8c894158b2d0229fc4268b2280da3f621e40f639
Louisejchi <99792428+Louisejchi@users.noreply.github.com>
2023-01-15 21:17:01 +0800

    Update divfreq.sv

--- divfreq.sv
+++ divfreq.sv
@@ -24,19 +24,6 @@ module divfreqForDrop (input CLK, output reg CLK_div);
 
 endmodule
 
-// for move
-module divfreqForMove (input CLK, output reg CLK_div);
-	reg [24:0] Count;
-	always @(posedge CLK)begin
-		if (Count > 2500000) begin
-			Count <= 25'b0;
-			CLK_div <= ~CLK_div;
-		end
-		else	Count <= Count + 1'b1;
-    end
-
-endmodule
-
 // for start animation
 module divfreqForStart (input CLK, output reg CLK_div);
 	reg [24:0] Count;
